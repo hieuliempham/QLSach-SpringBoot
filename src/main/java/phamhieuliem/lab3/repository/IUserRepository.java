bbffec79374e526a6a2ce7e8b18d463d10276b35
HieuLiem.Pham <hieuliem201@gmail.com>
2023-06-08 15:42:55 +0700

    Lab07: Phan quyen nguoi dung: ADMIN va USER

--- src/main/java/phamhieuliem/lab3/repository/IUserRepository.java
+++ src/main/java/phamhieuliem/lab3/repository/IUserRepository.java
@@ -9,21 +9,6 @@ import phamhieuliem.lab3.entity.User;
 
 @Repository
 public interface IUserRepository extends JpaRepository<User, String> {
-//    @Query("SELECT u FROM user u WHERE u.username = ?1")
-//    User findByUsername(String username);
-//
-//    @Modifying
-//    @Transactional
-//    @Query(value = "INSERT INTO user_role (user_id, role_id)" + "VALUES(?1, ?2)", nativeQuery = true)
-//    void addRoleToUser(Long userId, Long roleId);
-//
-//    @Query("SELECT u.id FROM User u WHERE u.username = ?1")
-//    Long getUserIdByUsername(String userName);
-//
-//    @Query(value = "SELECT r.name FROM role r INNER JOIN user_role ur " +
-//                    "ON r.id = ur.role_id WHERE ur.user_id = ?1", nativeQuery = true)
-//    String[] getRolesOfUser(Long userId);
-
     @Query("SELECT u FROM User u WHERE u.username = ?1")
     User findByUsername(String username);
     @Modifying
